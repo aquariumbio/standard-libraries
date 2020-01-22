fca5f9ac552a609fcc12dd1b0d9c6b31ef472bb3
Ben Keller <bjkeller@vt.edu>
2020-01-22 14:11:15 -0800

    Aquadoc initial commit

---
+++ js/definitions.js
@@ -0,0 +1,173 @@
+var config = {
+
+  tagline: "The Laboratory</br>Operating System",
+  documentation_url: "http://localhost:4000/aquarium",
+  title: "UW BIOFAB Standard Libraries",
+  navigation: [
+
+    {
+      category: "Overview",
+      contents: [
+        { name: "Introduction", type: "local-md", path: "README.md" },
+        { name: "About this Workflow", type: "local-md", path: "ABOUT.md" },
+        { name: "License", type: "local-md", path: "LICENSE.md" },
+        { name: "Issues", type: "external-link", path: 'https://github.com/klavinslab/standard-libraries/issues' }
+      ]
+    },
+
+    
+
+      {
+
+        category: "Operation Types",
+
+        contents: [
+
+          
+
+        ]
+
+      },
+
+    
+
+    
+
+      {
+
+        category: "Libraries",
+
+        contents: [
+
+          
+            {
+              name: 'Add Output',
+              path: 'libraries/Add_Output' + '.html',
+              type: "local-webpage"
+            },
+          
+            {
+              name: 'AssociationManagement',
+              path: 'libraries/AssociationManagement' + '.html',
+              type: "local-webpage"
+            },
+          
+            {
+              name: 'Centrifuge',
+              path: 'libraries/Centrifuge' + '.html',
+              type: "local-webpage"
+            },
+          
+            {
+              name: 'CommonInputOutputNames',
+              path: 'libraries/CommonInputOutputNames' + '.html',
+              type: "local-webpage"
+            },
+          
+            {
+              name: 'Debug',
+              path: 'libraries/Debug' + '.html',
+              type: "local-webpage"
+            },
+          
+            {
+              name: 'Feedback',
+              path: 'libraries/Feedback' + '.html',
+              type: "local-webpage"
+            },
+          
+            {
+              name: 'InputOutput',
+              path: 'libraries/InputOutput' + '.html',
+              type: "local-webpage"
+            },
+          
+            {
+              name: 'MatrixTools',
+              path: 'libraries/MatrixTools' + '.html',
+              type: "local-webpage"
+            },
+          
+            {
+              name: 'OperationErrors',
+              path: 'libraries/OperationErrors' + '.html',
+              type: "local-webpage"
+            },
+          
+            {
+              name: 'PlanParams',
+              path: 'libraries/PlanParams' + '.html',
+              type: "local-webpage"
+            },
+          
+            {
+              name: 'Preconditions',
+              path: 'libraries/Preconditions' + '.html',
+              type: "local-webpage"
+            },
+          
+            {
+              name: 'PrinterHelper',
+              path: 'libraries/PrinterHelper' + '.html',
+              type: "local-webpage"
+            },
+          
+            {
+              name: 'Priority Queue',
+              path: 'libraries/Priority_Queue' + '.html',
+              type: "local-webpage"
+            },
+          
+            {
+              name: 'ProvenanceFinder',
+              path: 'libraries/ProvenanceFinder' + '.html',
+              type: "local-webpage"
+            },
+          
+            {
+              name: 'SortHelper',
+              path: 'libraries/SortHelper' + '.html',
+              type: "local-webpage"
+            },
+          
+            {
+              name: 'TemporaryTubeLabels',
+              path: 'libraries/TemporaryTubeLabels' + '.html',
+              type: "local-webpage"
+            },
+          
+            {
+              name: 'Units',
+              path: 'libraries/Units' + '.html',
+              type: "local-webpage"
+            },
+          
+            {
+              name: 'UploadHelper',
+              path: 'libraries/UploadHelper' + '.html',
+              type: "local-webpage"
+            },
+          
+
+        ]
+
+    },
+
+    
+
+    
+      { category: "Sample Types",
+        contents: [
+          
+        ]
+      },
+      { category: "Containers",
+        contents: [
+          
+        ]
+      }
+    
+
+  ]
+
+};
