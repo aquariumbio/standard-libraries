3925d60de695a256c63c7d50950421ea2b705e39
Devin Strickland <dvnstrcklnd@users.noreply.github.com>
2020-05-25 10:44:29 -0700

    Aquadoc update

--- libraries/PlanParams.rb
+++ libraries/PlanParams.rb
@@ -6,10 +6,11 @@ module PlanParams
   # Gets `:options` from the `Plan` associations and the `Operations` and uses
   #   them to override `default_job_params`
   #
+  # @note
   # @param operations [OperationList] the operations
-  # @param default_job_params [Hash] the default parameters to be applied to all
-  #   `Operations` in the `Job`
-  # @return [Hash] the updated parameters to be applied to all
+  # @param default_job_params [Hash] the default parameters to be applied
+  #   equally to all `Operations` in the `Job`
+  # @return [Hash] the updated parameters to be applied equally to all
   #   `Operations` in the `Job`
   def update_job_params(operations:, default_job_params:)
     opts = strict_plan_options(operations)
@@ -48,13 +49,24 @@ module PlanParams
     operations.any? { |op| op.input('Options').try(:val).present? }
   end
 
-  # Gets :options from the plan associations and uses it to override
-  #   default_plan_params
+  # Updates a hash from options
   #
-  # @return [Hash] plan_params
+  # @param plan_params [Hash] the receiver hash
+  # @param opts [Hash] the donatng hash
+  # @return [Hash]
+  # @deprecate Use {#update_params} instead
   def update_plan_params(plan_params:, opts:)
-    plan_params.update(parse_options(opts)) if opts.present?
-    plan_params
+    update_params(default_params: plan_params, opts: opts)
+  end
+
+  # Updates a hash from options
+  #
+  # @param default_params [Hash] the receiver hash
+  # @param opts [Hash] the donatng hash
+  # @return [Hash]
+  def update_params(default_params:, opts:)
+    default_params.update(parse_options(opts)) if opts.present?
+    default_params
   end
 
   # Parses JSON formatted options
@@ -71,13 +83,36 @@ module PlanParams
   #
   # @param operations [OperationList] the operations
   # @param default_operation_params [Hash] the default parameters to be applied
-  #   to all `Operations` in the `Job`
+  #   to all `Operations` in the `Job` UNLESS overriden by input 'Options'
   # @return [void]
   def update_operation_params(operations:, default_operation_params:)
     operations.each do |op|
       opts = default_operation_params.dup.update(op.input('Options').val)
       op.temporary[:options] = opts
     end
+  end
+
+  # Convenience method for calling both {#update_job_params} and
+  #   {#update_operation_params}
+  #
+  # @param operations [OperationList] the operations
+  # @param default_job_params [Hash] the default parameters to be applied
+  #   equally to all `Operations` in the `Job`
+  # @param default_operation_params [Hash] the default parameters to be applied
+  #   to all `Operations` in the `Job` UNLESS overriden by input 'Options'
+  # @return [Hash] the updated parameters to be applied to all
+  #   `Operations` in the `Job`
+  def update_all_params(operations:, default_job_params:,
+                        default_operation_params:)
+    job_params = update_job_params(
+      operations: operations,
+      default_job_params: default_job_params
+    )
+    update_operation_params(
+      operations: operations,
+      default_operation_params: default_operation_params
+    )
+    job_params
   end
 
   ########## TESTING METHODS ##########
@@ -122,29 +157,10 @@ module PlanParams
     Operation.find(operations.map(&:id))
   end
 
-  private
-
-  # Update job_params based on options for the given operations
+  # Get an option value for a given key from the given operation
   #
-  def update_from_operations(operations:, job_params:)
-    job_params.keys.each do |key|
-      val = collate_options(operations: operations, key: key)
-      apply_update(job_params: job_params, key: key, val: val)
-    end
-    job_params
-  end
-
-  # Get a list of option values for a given key from the given operations list,
-  #   or raise exception if there is more than one unique value
-  #
-  def collate_options(operations:, key:)
-    val = operations.map { |op| get_option(operation: op, key: key) }
-    unless val.uniq.length == 1
-      msg = "More than one value given in Operation Options for #{key}:" \
-            " #{val}"
-      raise IncompatibleParametersError, msg
-    end
-    val.first
+  def get_options(operations:, key:)
+    operations.map { |op| get_option(operation: op, key: key) }
   end
 
   # Get an option value for a given key from the given operation
@@ -153,10 +169,29 @@ module PlanParams
     operation.input('Options').val.fetch(key, :no_key)
   end
 
-  # Update the job_params if the value is meaningful
+  private
+
+  # Update job_params based on options for the given operations
   #
-  def apply_update(job_params:, key:, val:)
-    job_params[key] = val unless val == :no_key
+  def update_from_operations(operations:, job_params:)
+    job_params.keys.each do |key|
+      val = strict_operations_option(operations: operations, key: key)
+      job_params[key] = val unless val == :no_key
+    end
+    job_params
+  end
+
+  # Get a list of option values for a given key from the given operations list,
+  #   or raise exception if there is more than one unique value
+  #
+  def strict_operations_option(operations:, key:)
+    val = get_options(operations: operations, key: key)
+    unless val.uniq.length == 1
+      msg = "More than one value given in Operation Options for #{key}:" \
+            " #{val}"
+      raise IncompatibleParametersError, msg
+    end
+    val.first
   end
 
   # Say you're quitting due to an error and error all the operations
